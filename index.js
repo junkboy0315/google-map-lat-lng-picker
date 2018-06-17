8e12b34783280c0c9b732fed2ca881172f6ce2dd
stam <junkboy0315@gmail.com>
2018-06-17 09:45:41 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(<div>testing</div>, document.querySelector('#root'));
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
